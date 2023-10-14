e7ba6f11cff4408bedddea9737c1a1e42d3276e7
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-10-14 22:47:16 +0330

    add user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from time import timezone
+from django.contrib.auth.hashers import make_password
+class UserManager(BaseUserManager):
 
-# Create your models here.
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username and password are required. Other fields are optional.
+    """
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+        db_index=True,
+    )
+    name = models.CharField('name', max_length=250, blank=False, null=False)
+    
+    email = models.EmailField('email address',unique=True, blank=False, db_index=True)
+    bio = models.CharField('bio', max_length=500, blank=True)
+    # Image = models.ImageField()
+    
+    is_staff = models.BooleanField(
+        'staff status',
+        default=False,
+        help_text='Designates whether the user can log into this admin site.',
+    )
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text=
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ,
+    )
+    date_joined = models.DateTimeField('date joined', default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_name(self):
+        """
+        Return the name.
+        """
+        return self.name
