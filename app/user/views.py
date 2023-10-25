756ae4e94758f1b4c96224d059763ccfdf1eb7ee
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-10-25 11:21:37 +0330

    remove authentication requirements of create user

--- app/user/views.py
+++ app/user/views.py
@@ -11,30 +11,13 @@ from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.generics import UpdateAPIView, RetrieveAPIView
 class CreateUserView(mixins.CreateModelMixin,
-                    mixins.UpdateModelMixin,
                       GenericViewSet,
                       ):
     serializer_class=UserCreateSerializer
-    authentication_classes=[JWTAuthentication,]
-    permission_classes=[IsAuthenticated,]
     queryset=get_user_model().objects.all()
 
-    def get_permissions(self):
-        """It returns no permission in case of creating new user (signing up)"""
-        if self.action=='create':
-            return []
-        return [permission() for permission in self.permission_classes]
-
     
     
-    
-    # @action(detail=False, url_name='userprofile', methods=['GET'])
-    # def userprofile(self, username):
-    #     """return specified users profile."""
-    #     instance=get_user_model().objects.get(username=username)
-    #     serializer=self.get_serializer(instance)
-        
-    #     return Response(serializer.data)
     
 class MyProfileManagerView(
     UpdateAPIView,
