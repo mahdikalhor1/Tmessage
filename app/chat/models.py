763c89362c27139a17915532ba419d0b00d9a1ea
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-11-09 23:12:04 +0330

    add pivate chat model

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth import get_user_model
+class Chat(models.Model):
 
-# Create your models here.
+    id=models.CharField('id', max_length=301,unique=True,
+                         validators=[UnicodeUsernameValidator,],
+                         db_index=True)
+#    messages=
+    users=models.ManyToManyField(to=get_user_model())
+
+class PrivateChat(Chat):
+
+    id=models.CharField('id', max_length=301,unique=True, db_index=True)
+    
+    def create_id(self):
+        username_1=self.users.get(0).username
+        username_2=self.users.get(1).username
+
+        if username_1 > username_2:
+            id=username_1 + '*' + username_2
+        else: 
+            id=username_2 + '*' + username_1
+
+        self.save()
